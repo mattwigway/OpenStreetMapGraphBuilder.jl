a06e16829992ce058af2f7955c8fecd0bb8fc11f
Matthew Wigginton Bhagat-Conway <matt@indicatrix.org>
2022-05-03 15:59:07 -0400

    align weights with OSRM

--- src/weights.jl
+++ src/weights.jl
@@ -3,15 +3,14 @@ const KMH_TO_MS = 1000 / 3600
 # short. Based on reading the code it seems likely that these are intended to be _decaseconds_ or
 # tens of seconds, and the docs are simply wrong
 # https://github.com/Project-OSRM/osrm-backend/issues/5989
-const TRAFFIC_LIGHT_PENALTY_SECS = 20.0
+const TRAFFIC_LIGHT_PENALTY_SECS = 2.0
 const BASE_TURN_PENALTY = 7.5
 # makes left turns more costly than right - for drive-on-left countries, set to
 # 1/1.075
 const TURN_BIAS = 1.075
 const TURN_PENALTY = 7.5
-
-# multiply all osm-provided maxspeeds by this
-const FREEFLOW_SPEED_REDUCTION = 0.8
+const U_TURN_PENALTY = 20.0
+const BASE_INTERSECTION_COST = 5.0  # This isn't in OSRM, but I think all intersection not on the motorway system should have some cost.
 
 # get the free flow weight (i.e. travel time, in seconds) for an edge
 # this is heavily based on the car.lua profile from OSRM v5.24.0
@@ -27,24 +26,22 @@ function compute_freeflow_weight(G, edge)
     # read the speed limit if it was in OSM, otherwise use default
     speed_kmh = get_prop(G, edge, :speed_kmh)
 
-    traffic_signal = get_prop(G, edge, :traffic_signal)::Int32
-
+    traffic_signal = get_prop(G, edge, :traffic_signal)
+    
     traversal_time_secs += length_m / (speed_kmh * KMH_TO_MS)
 
-    traversal_time_secs += TRAFFIC_LIGHT_PENALTY_SECS * traffic_signal
-
-    # now the turn time at the _end_ of the segment (turn time at start of segment already accounted for)
-    # TODO I need to add the concept of a "free flowing intersection" here where ways are split but there
-    # is no physical intersection
+    if traffic_signal
+        traversal_time_secs += TRAFFIC_LIGHT_PENALTY_SECS
+    end
 
     # first, determine if we even need a turn cost
     if ((rclass == OSM.motorway || rclass == OSM.motorway_link) &&
-        (next_rclass == OSM.motorway || next_rclass == OSM.motorway_link) && (traffic_signal == 0))
+        (next_rclass == OSM.motorway || next_rclass == OSM.motorway_link) && !traffic_signal)
         # no turn costs needed in the motorway system
         return traversal_time_secs, 0.0
     else
         # need to compute turn costs
-        turn_cost = 0.0      
+        turn_cost = BASE_INTERSECTION_COST  
 
         turn_angle = get_prop(G, edge, :turn_angle)
 
@@ -54,6 +51,10 @@ function compute_freeflow_weight(G, edge)
             turn_cost += TURN_PENALTY / (1 + exp( -((13 / TURN_BIAS) *  turn_angle/180 - 6.5*TURN_BIAS)))
         else
             turn_cost += TURN_PENALTY / (1 + exp( -((13 * TURN_BIAS) * -turn_angle/180 - 6.5/TURN_BIAS)))
+        end
+
+        if StreetRouter.OSM.is_turn_type(turn_angle, "u_turn")
+            turn_cost += U_TURN_PENALTY
         end
     end
 
